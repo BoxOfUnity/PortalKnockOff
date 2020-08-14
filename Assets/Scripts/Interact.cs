511a2086069ecef165a03b89a087d2561747d334
BoxOfUnity <Ace_Of_Fives@hotmail.com>
2020-08-13 20:55:09 -0400

    Debug interact edition.
    Changed interaction script so we can have a debug "movable check" override (bool in hierarchy in players interact script) so we can just, ya know, "YEET" the walls :D

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Interact : MonoBehaviour
 {
+    public bool overrideMovableCheck = false;
     public GameObject player;
     public float maxInteractDistance = 1.5f;
     private Camera playerCamera;
@@ -30,8 +31,17 @@ public class Interact : MonoBehaviour
                     {
                         if (hit.collider.gameObject.GetComponent<Pickupable>())
                         {
-                            Debug.Log("PICKEDEDUP A THINGY LIKE CUBE THINGI GUESS");
+                            Debug.Log("PICKEDEDUP A THINGY LIKE CUBE THING I GUESS");
                             heldObject = hit.collider.gameObject;
+                            heldObject.transform.parent = player.transform;
+                            //heldObjectStartPosition = heldObject.transform.position;
+                        }
+                        else if (overrideMovableCheck)
+                        {
+                            Debug.Log("PICKEDEDUP A THINGY LIKE CUBE THING I GUESS");
+
+                            heldObject = hit.collider.gameObject;
+                            Destroy(heldObject.GetComponent<Collider>());
                             heldObject.transform.parent = player.transform;
                             //heldObjectStartPosition = heldObject.transform.position;
                         }
@@ -41,16 +51,26 @@ public class Interact : MonoBehaviour
         }
         if (heldObject)
         {
-            heldObject.GetComponent<Rigidbody>().isKinematic = false;
-            heldObject.transform.position = Vector3.Lerp(heldObject.transform.position, playerCamera.transform.position + playerCamera.transform.forward * 1f, Time.deltaTime / pickupSpeed);
+            if (heldObject.GetComponent<Rigidbody>())
+            {
+                heldObject.GetComponent<Rigidbody>().isKinematic = true;
+            }
+            heldObject.transform.position = Vector3.Lerp(heldObject.transform.position, playerCamera.transform.position + playerCamera.transform.forward * 1.5f, Time.deltaTime / pickupSpeed);
             heldObject.transform.rotation = Quaternion.Lerp(heldObject.transform.rotation, playerCamera.transform.rotation, Time.deltaTime / pickupSpeed);
         }
         if (Input.GetButtonUp("Interact")&&heldObject != null)
         {
-
+            if (overrideMovableCheck)
+            {
+                heldObject.AddComponent<BoxCollider>();
+            }
             heldObject.transform.parent = null;
-            heldObject.GetComponent<Rigidbody>().isKinematic = true;
+            if (heldObject.GetComponent<Rigidbody>())
+            {
+                heldObject.GetComponent<Rigidbody>().isKinematic = false;
+            }
             heldObject = null;
+                
         }
 
     }
